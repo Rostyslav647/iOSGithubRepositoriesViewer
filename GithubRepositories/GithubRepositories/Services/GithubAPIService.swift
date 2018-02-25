2dd83fb0d0dc30c028eb4c113e0760c3a897214c
Ростислав <rostyslav.vasylyk@gmail.com>
2018-02-25 19:51:42 +0200

    Added getTags request

--- GithubRepositories/GithubRepositories/Services/GithubAPIService.swift
+++ GithubRepositories/GithubRepositories/Services/GithubAPIService.swift
@@ -14,12 +14,27 @@ class GithubAPIService {
     
     static let shared = GithubAPIService()
     
-    func getRepositories(_ completion: @escaping CompletionHandler) {
-        let decoder = JSONDecoder()
+    // Variables
+    let decoder = JSONDecoder()
+    
+    func getRepositories(_ completion: @escaping CompletionHandler<Repositories>) {
         
         Alamofire.request(GET_REPOSITORIES_URL).responseDecodableObject(decoder: decoder) {(response: DataResponse<Repositories>) in
             if let repo = response.result.value{
                 completion(repo)
+            } else {
+                debugPrint(response.result.error as Any)
+                completion(nil)
+            }
+        }
+    }
+    
+    func getTags(url: URL, completion: @escaping CompletionHandler<Tags>) {
+    
+        Alamofire.request(url).responseDecodableObject(decoder: decoder){
+            (response: DataResponse<Tags>) in
+            if let tags = response.result.value{
+                completion(tags)
             } else {
                 debugPrint(response.result.error as Any)
                 completion(nil)
