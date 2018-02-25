f7706af07540b8b0417a4d34cc1d8235de76f7de
Ростислав <rostyslav.vasylyk@gmail.com>
2018-02-25 17:11:45 +0200

    Implemented RepositoriesVC

--- GithubRepositories/GithubRepositories/Controller/RepositoriesVC.swift
+++ GithubRepositories/GithubRepositories/Controller/RepositoriesVC.swift
@@ -6,20 +6,65 @@
 //  Copyright © 2018 Ростислав. All rights reserved.
 //
 
-import UIKit
+import RxCocoa
+import RxSwift
+
 
 class RepositoriesVC: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var repositoriesTableView: UITableView!
+    
+    // Variables
+    var cellDescriptor = CellDescriptor(nibName: "RepositoryCell")
+    let repositoryViewModel = RepositoryViewModel()
+    let disposeBag = DisposeBag()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "Github Repositories"
+        cellDescriptor.register(in: repositoriesTableView)
+        addDelegate()
+        setupTableViewBinding()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func addDelegate(){
+        repositoriesTableView.delegate = nil
+        repositoriesTableView.dataSource = nil
     }
-
-
+    
+    func setupTableViewBinding() {
+        
+        repositoryViewModel.dataSourceObservable
+            .bind(to: self.repositoriesTableView.rx.items(cellIdentifier: cellDescriptor.identifier, cellType: RepositoryCell.self)) {  row, element, cell in
+                
+                cell.configure(element)
+            }
+            .disposed(by: self.disposeBag)
+        
+        repositoriesTableView.rx.itemSelected.map {
+            $0.item
+            }.subscribe(onNext: { [weak self] (index) in
+                let model = self?.repositoryViewModel.dataSource.value[index]
+                self?.repositoriesTableView.deselectRow(at: IndexPath.init(row: index, section: 0), animated: true)
+                self?.navigateToDetailedPage(model!)
+            })
+            .disposed(by:disposeBag)
+        
+    }
+    
+    func navigateToDetailedPage(_ model: Repository){
+        let repositoryDetailsVC = storyboard?.instantiateViewController(withIdentifier: "RepositoryDetailsVC") as! RepositoryDetailsVC
+        repositoryDetailsVC.configure(with: model)
+        self.navigationController?.pushViewController(repositoryDetailsVC, animated: true)
+    }
 }
 
+extension RepositoriesVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    
+}
