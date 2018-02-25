77a3663048a789651c1b365e3d52da2ec28790b1
Ростислав <rostyslav.vasylyk@gmail.com>
2018-02-25 17:18:29 +0200

    Added NibView

---
+++ GithubRepositories/GithubRepositories/View/NibView.swift
@@ -0,0 +1,44 @@
+//
+//  NibView.swift
+//  GithubRepositories
+//
+//  Created by Ростислав on 2/24/18.
+//  Copyright © 2018 Ростислав. All rights reserved.
+//
+
+import UIKit
+
+class NibView: UIView {
+    var view: UIView!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        nibSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        nibSetup()
+    }
+}
+
+private extension NibView {
+    
+    func nibSetup() {
+        
+        view = loadNib()
+        // use bounds not frame or it'll be offset
+        view.frame = bounds
+        // Adding custom subview on top of our view
+        addSubview(view)
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[childView]|",
+                                                      options: [],
+                                                      metrics: nil,
+                                                      views: ["childView": view]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[childView]|",
+                                                      options: [],
+                                                      metrics: nil,
+                                                      views: ["childView": view]))
+    }
+}
